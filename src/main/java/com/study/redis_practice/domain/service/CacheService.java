799c650cb6605d506af921d8b964efb4bd060399
ckstnr99 <ckstnr99@naver.com>
2024-11-30 10:01:35 +0900

    2024.11.30. 10:01 CacheService - deleteCacheData method write

--- src/main/java/com/study/redis_practice/domain/service/CacheService.java
+++ src/main/java/com/study/redis_practice/domain/service/CacheService.java
@@ -18,6 +18,7 @@ public class CacheService {
   private final RedisTemplate<String, Object> redisTemplate;
   private final ObjectMapper objectMapper;
 
+  // 데이터 캐싱
   public void cacheData(String key, Object data, long timeoutSeconds) {
     try {
       redisTemplate.opsForValue().set(key, data, timeoutSeconds, TimeUnit.SECONDS);
@@ -28,6 +29,7 @@ public class CacheService {
     }
   }
 
+  // 캐싱된 데이터 조회
   public <T> Optional<T> getCachedData(String key, Class<T> type) {
     try {
       Object data = redisTemplate.opsForValue().get(key);
@@ -36,6 +38,19 @@ public class CacheService {
       return Optional.of(objectMapper.convertValue(data, type));
     } catch (Exception exception) {
       return Optional.empty();
+    }
+  }
+
+  // 캐싱된 데이터 삭제
+  public void deleteCachedData(String key) {
+    try {
+      Boolean result = redisTemplate.delete(key);
+      if (Boolean.TRUE.equals(result)) log.info("Data deleted successfully for key: {}", key);
+      else log.warn("No data found for key: {}", key);
+
+    } catch (Exception exception) {
+      log.error("Error delete cached data: {}", exception.getMessage());
+      throw new RuntimeException("Delete cached operation failed", exception);
     }
   }
 
